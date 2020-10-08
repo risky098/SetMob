e94a32e4fbf73a9135fbdf849a7410cb2117d5e8
Develop when you have Time <70926890+risky098@users.noreply.github.com>
2020-10-08 12:36:42 +0900

    Add files via upload

--- SetMobRisky/src/main/java/risky/setmob/listener/Spawn.java
+++ SetMobRisky/src/main/java/risky/setmob/listener/Spawn.java
@@ -2,14 +2,17 @@ package risky.setmob.listener;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Zombie;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.inventory.ItemStack;
 import risky.setmob.Mob;
+
+import java.util.Set;
 
 public class Spawn implements Listener {
     private Mob plugin;
@@ -19,23 +22,28 @@ public class Spawn implements Listener {
         Bukkit.getPluginManager().registerEvents(this, plugin);
     }
 
-    @EventHandler
-    public void onMobSpawn(EntitySpawnEvent e) {
-        e.getEntity();
-        if(e.getEntityType() == EntityType.ZOMBIE) {
-            Zombie z = (Zombie)e.getEntity();
-            int v = plugin.getConfig().getInt("zombie.hp");
-            z.setMaxHealth(v);
-            z.setHealth(v);
-            z.setCanPickupItems(true);
-            z.setAdult();
-            z.getEquipment().setHelmet(new ItemStack(Material.getMaterial(plugin.getConfig().getString("zombie.helmet"))));
-            z.getEquipment().setChestplate(new ItemStack(Material.getMaterial(plugin.getConfig().getString("zombie.chestplate"))));
-            z.getEquipment().setLeggings(new ItemStack (Material.getMaterial (plugin.getConfig().getString ("zombie.leggings"))));
-            z.getEquipment().setBoots(new ItemStack(Material.getMaterial(plugin.getConfig().getString("zombie.boots"))));
-            z.getEquipment().setItemInMainHand(new ItemStack(Material.getMaterial(plugin.getConfig().getString("zombie.handitem"))));
-            if(z.getFireTicks() <= 1)
-                z.setFireTicks(0);
+    @EventHandler(priority =  EventPriority.NORMAL)
+    public void onMobSpawn(CreatureSpawnEvent e) {
+        LivingEntity ley = e.getEntity();
+        Set<String> monsters = plugin.getConfig().getConfigurationSection("monsters").getKeys(false);
+        String[] mList = monsters.<String>toArray(new String[monsters.size()]);
+        for(int i=0; i< monsters.size(); i++) {
+            String monster = mList[i];
+            if(ley.getType().equals(EntityType.valueOf(monster))) {
+                int v = plugin.getConfig().getInt("monsters." + mList[i] + ".hp");
+                ley.setMaxHealth(v);
+                ley.setHealth(v);
+                ItemStack helmet = new ItemStack(Material.getMaterial(plugin.getConfig().getString("monsters." + mList[i] + ".helmet")));
+                ley.getEquipment().setHelmet(helmet);
+                ItemStack chestplate = new ItemStack(Material.getMaterial(plugin.getConfig().getString("monsters." + mList[i] + ".chestplate")));
+                ley.getEquipment().setChestplate(chestplate);
+                ItemStack leggings = new ItemStack(Material.getMaterial(plugin.getConfig().getString("monsters." + mList[i] + ".leggings")));
+                ley.getEquipment().setLeggings(leggings);
+                ItemStack boots = new ItemStack(Material.getMaterial(plugin.getConfig().getString("monsters." + mList[i] + ".boots")));
+                ley.getEquipment().setBoots(boots);
+                ItemStack handitem = new ItemStack(Material.getMaterial(plugin.getConfig().getString("monsters." + mList[i] + ".handitem")));
+                ley.getEquipment().setItemInMainHand(handitem);
+            }
         }
     }
 }
